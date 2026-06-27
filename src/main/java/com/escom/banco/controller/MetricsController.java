4bb44bdf38db4751d505839e7956e7179ff4a736
edgaaaar <139918969+edgaaaar@users.noreply.github.com>
2026-06-27 13:44:48 -0600

    MetricsController.java

--- src/main/java/com/escom/banco/controller/MetricsController.java
+++ src/main/java/com/escom/banco/controller/MetricsController.java
@@ -3,6 +3,7 @@ package com.escom.banco.controller;
 import com.escom.banco.service.BancoService;
 import com.sun.management.OperatingSystemMXBean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.lang.management.ManagementFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class MetricsController {
