53b4512ad4ad639faa9847ab181aa077fded642e
edgaaaar <139918969+edgaaaar@users.noreply.github.com>
2026-06-27 13:45:34 -0600

    TransaccionController.java

--- src/main/java/com/escom/banco/controller/TransaccionController.java
+++ src/main/java/com/escom/banco/controller/TransaccionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class TransaccionController {
